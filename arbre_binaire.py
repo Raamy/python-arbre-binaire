34f6fc863d05d3405d302e32d05441c947809811
Raamy <ramy.benmamar@gmail.com>
2020-04-29 14:24:48 +0200

    Ajout getters/setters + str

--- arbre_binaire.py
+++ arbre_binaire.py
@@ -1,2 +1,34 @@
 class ArbreBinaire:
-    value = 0
+    valeur = 0
+    noeudGauche = 0
+    noeudDroit = 0
+
+    # ----- Getters et Setters -----
+    def getValeur(self):
+        return self.valeur
+
+    def getNoeudGauche(self):
+        return self.noeudGauche
+
+    def getNoeudDroit(self):
+        return self.noeudDroit
+
+    def setValeur(self, valeur):
+        self.valeur = valeur
+
+    def setNoeudGauche(self, noeudGauche):
+        self.noeudGauche = noeudGauche
+
+    def setNoeudDroit(self, noeudDroit):
+        self.noeudDroit = noeudDroit
+    # ------------------------------
+
+
+    # Affiche les données de l'ArbreBinaire()
+    def __str__(self):
+        return "\n----------" \
+               + "\nValeur du noeud : " + str(self.valeur) \
+               + "Sous-noeud Gauche : " + str(self.noeudGauche) \
+               + "Sous-noeud Droit : " + str(self.noeudDroit)
+
+
