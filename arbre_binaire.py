1c77eb6854a3e5eb7a9219df3d1f7d849acf8c42
Raamy <ramy.benmamar@gmail.com>
2020-04-29 14:45:37 +0200

    Ajout de classe TestArbreBinaire

--- arbre_binaire.py
+++ arbre_binaire.py
@@ -1,9 +1,10 @@
 class ArbreBinaire:
     valeur = 0
-    noeudGauche = 0
-    noeudDroit = 0
+    noeudGauche = None
+    noeudDroit = None
 
     # ----- Getters et Setters -----
+
     def getValeur(self):
         return self.valeur
 
@@ -21,14 +22,24 @@ class ArbreBinaire:
 
     def setNoeudDroit(self, noeudDroit):
         self.noeudDroit = noeudDroit
-    # ------------------------------
 
+    # ------------------------------
 
     # Affiche les données de l'ArbreBinaire()
     def __str__(self):
-        return "\n----------" \
+        return "\n----- Données du noeud -----" \
                + "\nValeur du noeud : " + str(self.valeur) \
-               + "Sous-noeud Gauche : " + str(self.noeudGauche) \
-               + "Sous-noeud Droit : " + str(self.noeudDroit)
+               + "\nSous-noeud Gauche : " + str(self.noeudGauche) \
+               + "\nSous-noeud Droit : " + str(self.noeudDroit)
 
 
+class TestArbreBinaire:
+    node: ArbreBinaire
+
+    def __init__(self, valeur):
+        self.node = ArbreBinaire()
+        self.node.setValeur(valeur)
+        print(self.node)
+
+
+testArbreBinaire = TestArbreBinaire(10)
